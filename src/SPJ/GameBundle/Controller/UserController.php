f2a6a794a6d2aa2bac24fc89b7abdfa05ef5ff06
oservieres <oservieres@mediastay.com>
2013-09-29 03:11:43 +0200

    added signup form

--- src/SPJ/GameBundle/Controller/UserController.php
+++ src/SPJ/GameBundle/Controller/UserController.php
@@ -4,12 +4,30 @@ namespace SPJ\GameBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use SPJ\GameBundle\Entity\User;
+use SPJ\GameBundle\Form\UserType;
+
 class UserController extends Controller
 {
     public function loginAction()
     {
         return $this->render(
             'SPJGameBundle:User:login.html.twig'
+        );
+    }
+
+    public function signupAction()
+    {
+        $form = $this->createForm(new UserType(), new User(), array(
+            'action' => $this->generateUrl('signup_check'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $this->render(
+            'SPJGameBundle:User:signup.html.twig',
+            array('form' => $form)
         );
     }
 
