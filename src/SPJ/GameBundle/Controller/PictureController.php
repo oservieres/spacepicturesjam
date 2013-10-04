98a2e3b606faba7cc1a5e856db54d591fd82f829
oservieres <oservieres@mediastay.com>
2013-10-04 22:52:14 +0200

    added picture upload form

--- src/SPJ/GameBundle/Controller/PictureController.php
+++ src/SPJ/GameBundle/Controller/PictureController.php
@@ -2,15 +2,40 @@
 
 namespace SPJ\GameBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use SPJ\GameBundle\Entity\Picture;
+use SPJ\GameBundle\Form\PictureType;
 
 class PictureController extends Controller
 {
-    public function createAction()
+    public function createAction(Request $request, $challengeId)
     {
-        return $this->render(
-            'SPJGameBundle:Picture:create.html.twig'
-        );
+        $picture = new Picture();
+        $form = $this->createForm(new PictureType(), $picture, array(
+            'action' => $this->generateUrl(
+                'picture_create',
+                array('challengeId' =>  $challengeId)
+            ),
+            'method' => 'POST',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($picture);
+            $em->flush();
+
+            return $this->render('SPJGameBundle:Picture:created.html.twig');
+        }
+
+        return $this->render('SPJGameBundle:Picture:create.html.twig', array(
+            'picture' => $picture,
+            'form'   => $form->createView(),
+        ));
     }
 
 }
---
+++ src/SPJ/GameBundle/Form/PictureType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SPJ\GameBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PictureType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('location')
+            ->add('description')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SPJ\GameBundle\Entity\Picture'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'spj_gamebundle_picture';
+    }
+}
