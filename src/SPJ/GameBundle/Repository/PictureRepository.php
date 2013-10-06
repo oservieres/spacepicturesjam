aa7416ca47e4075f170b05a444ccd0cfd5ef96f4
oservieres <oservieres@mediastay.com>
2013-10-06 20:30:29 +0200

    fixed picture display when no one was sent

--- src/SPJ/GameBundle/Repository/PictureRepository.php
+++ src/SPJ/GameBundle/Repository/PictureRepository.php
@@ -2,6 +2,7 @@
 namespace SPJ\GameBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use \Doctrine\Orm\NoResultException;
 
 /**
  * PictureRepository
@@ -10,13 +11,17 @@ class PictureRepository extends EntityRepository
 {
     public function findOneByChallengeAndUser($challenge, $user)
     {
-        return $this->createQueryBuilder('picture')
-                    ->where('picture.challenge = :challenge')
-                    ->setParameter('challenge', $challenge)
-                    ->andWhere('picture.user = :user')
-                    ->setParameter('user', $user)
-                    ->setMaxResults(1)
-                    ->getQuery()
-                    ->getSingleResult();
+        try {
+            return $this->createQueryBuilder('picture')
+                        ->where('picture.challenge = :challenge')
+                        ->setParameter('challenge', $challenge)
+                        ->andWhere('picture.user = :user')
+                        ->setParameter('user', $user)
+                        ->setMaxResults(1)
+                        ->getQuery()
+                        ->getSingleResult();
+        } catch (NoResultException $exception) {
+            return null;
+        }
     }
 }
