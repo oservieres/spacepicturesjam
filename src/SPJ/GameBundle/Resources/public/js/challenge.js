bfaa07739c13b6a71d683b460f9d3f3b0d467ea0
oservieres <oservieres@mediastay.com>
2013-10-06 22:25:03 +0200

    refresh challenge when picture is posted

--- src/SPJ/GameBundle/Resources/public/js/challenge.js
+++ src/SPJ/GameBundle/Resources/public/js/challenge.js
@@ -3,18 +3,17 @@ spacePicturesJam.challenge = {};
 spacePicturesJam.challenge.init = function() {
     $('#picture_upload_button').click(spacePicturesJam.challenge.displayUploadForm);
 
-    spacePicturesJam.challenge.loadPictures();
+    $('.challenge_pictures').each(function() {
+        spacePicturesJam.challenge.loadPictures($(this));
+    });
 }
 
-spacePicturesJam.challenge.loadPictures = function() {
-    $('.challenge_pictures').each(function() {
-        var picturesContainer = $(this);
-        $.ajax({
-            url : picturesContainer.attr('data-pictures_url'),
-            dataType: 'html'
-        }).done(function(response) {
-            picturesContainer.html(response);
-        });
+spacePicturesJam.challenge.loadPictures = function(picturesContainer) {
+    $.ajax({
+        url : picturesContainer.attr('data-pictures_url'),
+        dataType: 'html'
+    }).done(function(response) {
+        picturesContainer.html(response);
     });
 };
 
@@ -25,6 +24,11 @@ spacePicturesJam.challenge.displayUploadForm = function(event) {
         href: $('#picture_upload_button').attr('data-href'),
         type: 'iframe'
     });
+}
+
+spacePicturesJam.challenge.pictureCreatedCallback = function() {
+    spacePicturesJam.challenge.loadPictures($('.challenge_pictures.inprogress'));
+    $.fancybox.close();
 }
 
 spacePicturesJam.challenge.init();
