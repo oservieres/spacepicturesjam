178de227dfade95b0a8c507d41b0e3e0b5b157aa
oservieres <oservieres@mediastay.com>
2013-10-09 02:25:01 +0200

    add parameters for grub

--- script/vagrant-install.sh
+++ script/vagrant-install.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/bin/bash -e
+
+sudo apt-get update -y
+
+echo grub-pc grub-pc/install_devices multiselect /dev/sda | sudo debconf-set-selections
+echo grub-pc grub-pc/install_devices_disks_changed multiselect /dev/sda | sudo debconf-set-selections
+
+sudo apt-get upgrade -y -o dir::cache::archives="/vagrant/logs/apt-cache"
 
 echo mysql-server-5.5 mysql-server/root_password password root | sudo debconf-set-selections
 echo mysql-server-5.5 mysql-server/root_password_again password root | sudo debconf-set-selections
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-
-sudo apt-get install mysql-server mysql-client
+sudo apt-get install -y -o dir::cache::archives="/vagrant/logs/apt-cache" mysql-server-5.5 mysql-client-5.5
 echo "create database spacepicturesjam ; GRANT ALL PRIVILEGES ON spacepicturesjam.* To 'spacepicturesjam'@'localhost' IDENTIFIED BY 'spacepicturesjam' ;" | mysql -u root -proot
 
-sudo apt-get install -fy tree curl apache2 php5-mysql php5 php5-cli php-pear php5-curl phpunit php5-intl php5-dev php5-gd php5-mcrypt git-core git
+sudo apt-get install -y -o dir::cache::archives="/vagrant/logs/apt-cache" tree curl apache2 php5-mysql php5 php5-cli php-pear php5-curl phpunit php5-intl php5-dev php5-gd php5-mcrypt git-core git
 
 sudo cp /vagrant/etc/apache_local.conf /etc/apache2/sites-available/spacepicturesjam
 ln -s parameters.yml.dist /vagrant/app/config/parameters.yml
