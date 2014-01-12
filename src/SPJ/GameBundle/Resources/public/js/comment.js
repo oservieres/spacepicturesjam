bc527821efa4e27323c6837df8725ac88c510eab
oservieres <oservieres@mediastay.com>
2014-01-12 01:21:50 +0100

    prevetn form double submit

--- src/SPJ/GameBundle/Resources/public/js/comment.js
+++ src/SPJ/GameBundle/Resources/public/js/comment.js
@@ -1,9 +1,23 @@
 spacePicturesJam.comment = {};
 
+spacePicturesJam.comment.lockForm = function(commentForm) {
+    if ('true' == commentForm.attr('is_locked')) {
+        return false;
+    }
+    commentForm.attr('is_locked', 'true');
+    return true
+};
+
+spacePicturesJam.comment.unlockForm = function(commentForm) {
+    commentForm.attr('is_locked', 'false');
+};
 spacePicturesJam.comment.initForm = function(commentForm) {
     var challengeId = commentForm.attr('data-challenge_id');
     commentForm.find('.content').keypress(function(e) {
         if (13 != e.which) {
+            return;
+        }
+        if (!spacePicturesJam.comment.lockForm(commentForm)) {
             return;
         }
         e.preventDefault();
@@ -13,6 +27,8 @@ spacePicturesJam.comment.initForm = function(commentForm) {
             data : commentForm.serialize(),
             dataType: 'json'
         }).done(function(response) {
+            commentForm.find('.content').val('');
+            spacePicturesJam.comment.unlockForm(commentForm)
             $("#challenge_" + challengeId + "_comments ul.list-group").animate({
                 scrollTop: $("#challenge_" + challengeId + "_comments ul.list-group")[0].scrollHeight
             }, 300);
@@ -22,7 +38,6 @@ spacePicturesJam.comment.initForm = function(commentForm) {
             var dateContainer = $('<span></span>').html(response.data.comment.date_created);
             commentContainer.append($('<p class="date"></p>').append(dateContainer));
             $("#challenge_" + challengeId + "_comments ul").append(commentContainer);
-            commentForm.find('.content').val('');
         });
     });
 }
